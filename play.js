1f532d6641e7af56b4648c8d19f66ce05f9443a8
Leshan Patterson <leshan0627@gmail.com>
2021-12-01 23:13:56 +0000

    Origin master

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: "50541",
+  });
+
+  // interpret incoming data as text and console.log it
+   conn.on("connect", () => {
+    console.log(conn.setEncoding("utf8"))
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
